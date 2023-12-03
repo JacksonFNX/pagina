3c458ebdff87f9c33d9b577ce89c8f4dabb1948e
JacksonFNX <85742256+JacksonFNX@users.noreply.github.com>
2023-12-03 18:32:36 -0300

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,118 @@
-import logo from './logo.svg';
+import tfroma from './Images/not1.png';
+import iss from './Images/iss.jpg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="HomePage">
+      <header id="Artigopg">
+        <div className="Menupg">
+          <h1>CosmosExplorer</h1>
+          <div className="menubotoes">
+          <ul>
+                  <li><a href="/">Início</a> </li>
+                  <li><a href="/home#Projects">Artigos</a> </li>
+                  <li><a href="#Sobre">Sobre</a> </li>
+              </ul>
+          </div>
+        </div>
+        <div className="stars"></div>
+        <div className="stars2"></div>
+        <h1 className="titulopg"><br/>Como funciona a Terraformação?</h1>
+          <div className="container1">
+            <div className="Destaquepg">
+            <img src={tfroma} className="foto" alt="foto" />
+              <h2>
+              Terraformação: o que é e como a geoengenharia pode ajudar a tornar planetas habitáveis?
+              </h2>
+                <p className ="paragrafopg">
+                &nbsp;A busca por novos mundos habitáveis tem sido um dos maiores desafios da humanidade desde tempos imemoriais. Através da ficção científica, o ser humano explorou diversas formas de colonizar e tornar habitáveis outros planetas. A ideia de modificar a geologia, atmosfera e ecologia de corpos celestes inóspitos para torná-los adequados à vida humana é conhecida como terraformação.
+                </p>
+                <p className ="paragrafopg">
+                &nbsp;No entanto, a terraformação permanece em grande parte no domínio da especulação, pois as dificuldades técnicas e os custos astronômicos envolvidos são proibitivos. Mas e se a geoengenharia, a manipulação deliberada dos sistemas terrestres, puder oferecer insights e soluções para esse desafio cósmico? Neste texto, exploraremos o conceito de terraformação, seus desafios e, principalmente, como a geoengenharia pode contribuir para a realização desse sonho humano.
+                </p>
+                <h2>O que é Terraformação?</h2>
+                <p className ="paragrafopg">
+                &nbsp;A terraformação, também conhecida como “geoengenharia planetária”, é a denominação dada ao processo (até agora hipotético) de modificar a atmosfera, geologia e ecossistemas de um planeta, lua ou outro corpo celestial, tornando-o adequado para a vida humana ou outras formas de vida terrestre. A ideia é transformar um ambiente hostil em um espaço onde seres humanos possam viver, respirar e prosperar, sem a necessidade de trajes espaciais ou cápsulas de sobrevivência. Basicamente, formar uma nova Terra.
+                </p>
+                <p className ="paragrafopg">
+                &nbsp;A terraformação é um tema recorrente na ficção científica, explorado em filmes, livros, quadrinhos e jogos, não sendo inclusive algo recente, já sendo tema de história a muito tempo atrás. Apesar de ser um processo hipotético, diversas obras trabalham com teorias que poderiam ser aplicáveis no mundo real.
+                </p>
+                <h2>Os desafios desse processo</h2>
+                <p className ="paragrafopg">
+                &nbsp;A terraformação é uma tarefa hercúlea, com desafios técnicos, temporais e financeiros extraordinários, o que justifica, por exemplo, o fato de ser até o momento algo presente apenas no campo da teoria. Entre alguns dos desafios incluídos no possível processo estão:
+                </p>
+                <p></p>
+                <ul><li><strong>Atmosfera inadequada:</strong> Muitos planetas e luas carecem de atmosferas, ou suas atmosferas são compostas por gases tóxicos para os humanos, como dióxido de enxofre ou metano.</li><li><strong>Temperatura extrema:</strong> A maioria dos planetas em nosso sistema solar possui temperaturas extremas que variam de centenas de graus negativos a positivos. A estabilidade térmica é crucial para a vida.</li><li><strong>Radiação cósmica:</strong> Sem a proteção de uma atmosfera adequada, a radiação cósmica mortal se torna uma ameaça constante à vida.</li><li><strong>Pressão atmosférica:</strong> Para que os seres humanos possam viver em um planeta, a pressão atmosférica deve ser adequada para manter a água em estado líquido, o que é essencial para a vida como a conhecemos.</li><li><strong>Solo e recursos naturais:</strong> A disponibilidade de solo fértil, água, minerais e outros recursos é essencial para a sobrevivência de qualquer colônia planetária.</li><li><strong>Biodiversidade:</strong> A introdução de uma variedade de organismos terrestres em um novo ambiente deve ser cuidadosamente planejada para evitar desequilíbrios ecológicos.</li></ul>
+                <h2>O que é Geoengenharia?</h2>
+                <p className ="paragrafopg">
+                &nbsp;A geoengenharia é o campo interdisciplinar que se concentra na manipulação deliberada dos sistemas terrestres para atender às necessidades da sociedade ou para mitigar os impactos das mudanças climáticas. Embora a geoengenharia tenha suas raízes em problemas terrestres, suas técnicas e princípios podem ter aplicações surpreendentes na terraformação de planetas e luas distantes.
+                </p>
+                <p className ="paragrafopg">
+                &nbsp;A geoengenharia compreende uma variedade de técnicas e abordagens, das quais algumas podem ser diretamente aplicadas à terraformação planetária. A captura de carbono é uma dessas técnicas, por exemplo, que envolve a remoção de dióxido de carbono (CO₂) da atmosfera. Essa técnica é relevante para a terraformação, pois a regulação da atmosfera de um planeta é fundamental para criar condições habitáveis.
+                </p>
+                <p className ="paragrafopg">
+                &nbsp;Espelhos espaciais são um outro tipo de abordagem a ser mencionado, já que são dispositivos que refletem a luz solar de volta ao espaço, ajudando a controlar a temperatura de um planeta ou a direcionar a luz para áreas específicas. Isso pode ser usado para aquecer um planeta frio ou esfriar um muito quente basicamente, se aplicado no conceito de terraformação.
+                </p>
+                <img src={iss} className="foto" alt="foto" />
+                <p className ="paragrafopg">
+                &nbsp;Um dos principais desafios citados pela terraformação é em relação às atmosferas dos planetas e nesse caso, a geoengenharia pode envolver a introdução de gases ou partículas específicas na atmosfera para alterar suas propriedades. Isso pode ser usado para ajustar a pressão atmosférica e a composição de gases em um planeta.
+                </p>
+                <p className ="paragrafopg">
+                &nbsp;Por fim, a introdução de microrganismos terrestres pode desempenhar um papel fundamental na modificação da geologia e ecologia de um planeta. Além disso, esses microrganismos podem desempenhar um papel na produção de oxigênio e na remoção de substâncias tóxicas.
+                </p>
+                <p className ="paragrafopg">
+                &nbsp;Esses são apenas alguns dos caminhos possíveis a serem percorridos e algumas das abordagens e processos que poderiam ser aplicáveis no conceito de Terraformação.
+                </p>
+                <h2>Estudos e experimentos em Geoengenharia Planetária</h2>
+                <p className ="paragrafopg">
+                &nbsp;Antes de considerar a aplicação da geoengenharia em outros planetas, os cientistas estão conduzindo experimentos na Terra para entender melhor as implicações e os efeitos das técnicas propostas. Isso inclui experimentos com espelhos espaciais, injeção de aerossóis estratosféricos e outros métodos.
+                </p>
+                <p className ="paragrafopg">
+                &nbsp;Além dos experimentos práticos, simulações computacionais desempenham um papel fundamental na modelagem de como as técnicas de geoengenharia podem afetar a atmosfera, a temperatura e outros parâmetros em planetas específicos.
+                </p>
+                <p className ="paragrafopg">
+                &nbsp;Dada a natureza global das mudanças climáticas e da geoengenharia, é imperativo que os esforços sejam coordenados internacionalmente. Iniciativas como a Convenção sobre Diversidade Biológica (CDB) estão explorando questões éticas e regulatórias relacionadas à geoengenharia.
+                </p>
+                <h2>Desafios e controvérsias na Terraformação e Geoengenharia</h2>
+                <p className ="paragrafopg">
+                &nbsp;Apesar dos avanços, muitos desafios técnicos persistem. A introdução de microrganismos, por exemplo, pode resultar em reações imprevisíveis, e o controle preciso da atmosfera de um planeta é uma tarefa monumental. Sem contar todos os outros inúmeros tipos de problemas que podem surgir ao modificar tão drasticamente qualquer planeta ou ambiente que seja, podendo gerar desequilíbrios consideráveis na estrutura já existente e nas que venham a se formar.
+                </p>
+                <p className ="paragrafopg">
+                &nbsp;Além disso, existe ainda todo um debate ético em relação à terraformação, com muitas entidades relacionando os estudos desse tipo de processo e também da geoengenharia como “alguém brincando de ser Deus”. Outras implicações como quem decide qual planeta deve ou não ser terraformado, de que forma seria feito e qual o objetivo final também são pontos considerados no debate.
+                </p>
+                <p className ="paragrafopg">
+                &nbsp;Por hora, a terraformação permanece no reino da ficção científica, mas a geoengenharia está fornecendo insights valiosos sobre como podemos manipular os sistemas planetários para tornar mundos distantes habitáveis.
+                </p>
+                <p className ="paragrafopg">
+                &nbsp;Embora muitos desafios técnicos e éticos permaneçam, o avanço contínuo na geoengenharia pode eventualmente abrir as portas para a realização da humanidade de colonizar e habitar outros planetas. À medida que continuamos a explorar o cosmos, a terraformação e a geoengenharia podem se tornar ferramentas cruciais para transformar o desconhecido em algo familiar.
+                </p>
+            </div>
+          </div>
       </header>
+      <div id= "Sobre">
+              <h2 className="titulopg">Sobre nós</h2>
+              <br/>
+              <br/>
+              <p>
+              Projeto academico da disciplina de Interfaces para Mídias Digitais
+              </p>
+              <br/>
+              <p>
+              Jackson Galdino 26423375
+              </p>
+              <br/>
+              <p>
+              Discord: fnx_
+              </p>
+              <br/>
+              <p>
+              Email: jacksonfnx@gmail.com
+              </p>
+              <br/>
+              <p>
+              GitHub /JacksonFNX
+              </p>
+      </div>
     </div>
   );
 }
